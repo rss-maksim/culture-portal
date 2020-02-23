e79f180296ca09d19e695cdad3fb8e44de07907b
truhin-andrei <truhin-andrei@bk.ru>
2020-02-23 21:20:53 +0700

    feat: container for map

--- src/pages/director.jsx
+++ src/pages/director.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable array-bracket-spacing */
-import React, { useState, useEffect } from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
-import Grid from '@material-ui/core/Grid'
 import { useMediaQuery } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+import DirectorCard from '../components/DirectorCard/DirectorCard'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import DirectorCard from '../components/DirectorCard/DirectorCard'
 import TableOfWorks from '../components/TableOfWorks/TableOfWorks'
-
+import Map from '../components/Map/Map'
 import './director.css'
 
 const useStyles = makeStyles(theme => ({
@@ -97,6 +97,7 @@ const DirectorPage = () => {
           className="director-info"
         >
           <TableOfWorks work={directorData.works} />
+          <Map />
         </Grid>
       </Grid>
 
