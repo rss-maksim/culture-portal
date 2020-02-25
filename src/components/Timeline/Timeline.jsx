332e1f6dba2e14362842a3e2219145ddf8de4549
Ivan Pastukhov <spirithntr@gmail.com>
2020-02-25 23:56:31 +0400

    refactor: fix after code review
    move styles from templates
    apply colors from theme

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,37 +4,42 @@ import {
   Timeline as TimelineComponent,
   TimelineItem
 } from 'vertical-timeline-component-for-react'
-import { capitalize } from '../../utils/utils'
+import { capitalize } from '../../utils'
+import { useTheme } from '@material-ui/core/styles'
 
-// TODO: change colors according to style guide
-const primaryColor = '#243665'
-const complementaryColor = '#8BD8BD'
-const filler = '#ddd'
-const Timeline = ({ biography }) => (
-  <TimelineComponent lineColor={filler}>
-    {biography.map((event, index) => (
-      <TimelineItem
-        key={index}
-        dateText={event.date}
-        style={{ color: complementaryColor }}
-        dateInnerStyle={{
-          background: primaryColor,
-          color: complementaryColor,
-          'padding-left': '20px'
-        }}
-        bodyContainerStyle={{
-          background: filler,
-          color: primaryColor,
-          padding: '20px',
-          borderRadius: '8px',
-          boxShadow: '0.2rem rgba(0, 0, 0, 0.2)'
-        }}
-      >
-        {capitalize(event.description)}
-      </TimelineItem>
-    ))}
-  </TimelineComponent>
-)
+const Timeline = ({ biography }) => {
+  const {
+    palette: { primary, secondary, grey }
+  } = useTheme()
+  const itemStyle = { color: primary.main }
+  const dateStyle = {
+    background: primary.main,
+    color: primary.contrastText,
+    paddingLeft: '20px'
+  }
+  const bodyStyle = {
+    background: grey[300],
+    color: primary.main,
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0.2rem rgba(0, 0, 0, 0.2)'
+  }
+  return (
+    <TimelineComponent lineColor={grey[300]}>
+      {biography.map((event, index) => (
+        <TimelineItem
+          key={index}
+          dateText={event.date}
+          style={itemStyle}
+          dateInnerStyle={dateStyle}
+          bodyContainerStyle={bodyStyle}
+        >
+          {capitalize(event.description)}
+        </TimelineItem>
+      ))}
+    </TimelineComponent>
+  )
+}
 Timeline.propTypes = {
   biography: PropTypes.arrayOf(
     PropTypes.shape({
